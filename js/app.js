78af251c1add3ab33266262a544e879a111b7fe8
Héctor Palma Téllez <hector@palmatellez.cl>
2014-09-25 10:53:21 -0300

    Creo que corrige lo de los paneles.

--- js/app.js
+++ js/app.js
@@ -3,11 +3,18 @@ $(document).ready(function(){
     $("#panel-contacto").slideToggle("fast");
   });
 });
-$('h1.datos.x').click(function(){ $('#panel-contacto').slideToggle("fast") });
+$('h1.datos.x').click(function(){
+  $('#panel-contacto').slideToggle("fast");
+  $('#panel-yo').fadeOut('fast');
+});
 
 $(document).ready(function(){
   $(".sobremi").click(function(){
     $("#panel-yo").slideToggle("fast");
+    $('#panel-contacto').fadeOut('fast');
   });
 });
-$('h1.datos.ex').click(function(){ $('#panel-yo').slideToggle("fast") });
+$('h1.datos.ex').click(function(){
+  $('#panel-yo').slideToggle("fast");
+  $('#panel-contacto').fadeOut('fast');
+});
